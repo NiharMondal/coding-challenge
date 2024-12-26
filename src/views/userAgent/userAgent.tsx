51cc6ec725661f65f16d0e1797464441e413230d
Nihar Mondal <developernihar@gmail.com>
2024-12-26 17:24:41 +0600

    Fix: Implemented challenges

--- src/views/userAgent/userAgent.tsx
+++ src/views/userAgent/userAgent.tsx
@@ -2,23 +2,29 @@
 
 import { BackToHome } from "@/components/backToHome/backToHome";
 import { useUserAgentContext } from "@/components/providers/userAgentProvider";
+import { useEffect, useState } from "react";
+export const UserAgent = ({ serverUserAgent }: { serverUserAgent: string }) => {
+	const { userAgent: clientUserAgent } = useUserAgentContext();
+	const [userAgent, setUserAgent] = useState<string | null>(serverUserAgent);
 
-export const UserAgent = () => {
-  const { userAgent } = useUserAgentContext();
+	useEffect(() => {
+		if (clientUserAgent) {
+			setUserAgent(clientUserAgent);
+		}
+	}, [clientUserAgent]);
 
-  return (
-    <div>
-      <BackToHome />
+	return (
+		<div>
+			<BackToHome />
 
-      {userAgent && (
-        <div className="flex font-mono font-semibold text-sm">
-          <div className="border p-2">UserAgent</div>
-
-          <div className="border p-2">{userAgent}</div>
-        </div>
-      )}
-
-      {!userAgent && <div>No user agent</div>}
-    </div>
-  );
+			{userAgent ? (
+				<div className="flex font-mono font-semibold text-sm">
+					<div className="border p-2">UserAgent</div>
+					<div className="border p-2">{userAgent}</div>
+				</div>
+			) : (
+				<div>No user agent</div>
+			)}
+		</div>
+	);
 };
